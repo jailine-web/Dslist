3a3f4642a94d34f1d6397fa1726d9edfac8a71ab
Jailine <jay.santana964@gmail.com>
2023-05-10 16:44:23 -0300

    Buscar por id - GameDTo

--- Dslist/src/main/java/com/ProjetoJava/Dslist/services/GameService.java
+++ Dslist/src/main/java/com/ProjetoJava/Dslist/services/GameService.java
@@ -4,21 +4,30 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ProjetoJava.Dslist.DTO.GameDTO;
 import com.ProjetoJava.Dslist.DTO.GameMinDTO;
 import com.ProjetoJava.Dslist.entities.Game;
 import com.ProjetoJava.Dslist.repositories.GameRepository;
 
-@Service //registra a classe como um componente no sistema, ele pode injetar um componente no outro
+@Service 
 public class GameService {
 
 	@Autowired //injetando uma instância do gameRepository na classe gameService
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> buscarTodosOsGames(){
 		List<Game> listaGames = gameRepository.findAll();
 		List<GameMinDTO> gamesDTO = listaGames.stream().map(x -> new GameMinDTO(x)).toList();
 		return gamesDTO;
+	}
+	
+	public GameDTO buscarPorId(Long id){
+		Game resposta = gameRepository.findById(id).get();
+		GameDTO gameDto = new GameDTO(resposta);
+		return gameDto; 
 	}
 	
 }
