a5e4a1a29adf000a14a2b00934e137fac3b9905a
Jailine <jay.santana964@gmail.com>
2023-05-18 18:07:35 -0300

    Mover e reordenar filmes na lista

--- Dslist/src/main/java/com/ProjetoJava/Dslist/controllers/GameListController.java
+++ Dslist/src/main/java/com/ProjetoJava/Dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ProjetoJava.Dslist.DTO.GameListDTO;
 import com.ProjetoJava.Dslist.DTO.GameMinDTO;
+import com.ProjetoJava.Dslist.DTO.SubstituicaoDTO;
 import com.ProjetoJava.Dslist.services.GameListService;
 import com.ProjetoJava.Dslist.services.GameService;
 
@@ -33,6 +36,14 @@ public class GameListController {
 	public List<GameMinDTO> buscarListas(@PathVariable Long idLista){
 		List<GameMinDTO> listas = gameService.buscarPorListas(idLista);
 		return listas;
+	}
+	
+	@PostMapping(value = "/{idLista}/substituicao")
+	public void mover(@PathVariable Long idLista, @RequestBody SubstituicaoDTO infos){
+		
+		//recebe o corpo da requisição 
+		gameListService.moverFilme(idLista, infos.getIndiceOrigem(), infos.getIndiceDestino());
+		
 	}
 	
 	
--- Dslist/src/main/java/com/ProjetoJava/Dslist/services/GameListService.java
+++ Dslist/src/main/java/com/ProjetoJava/Dslist/services/GameListService.java
@@ -7,15 +7,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ProjetoJava.Dslist.DTO.GameListDTO;
-import com.ProjetoJava.Dslist.DTO.GameMinDTO;
 import com.ProjetoJava.Dslist.entities.GameList;
+import com.ProjetoJava.Dslist.projetos.ProjetoGame;
 import com.ProjetoJava.Dslist.repositories.GameListRepository;
+import com.ProjetoJava.Dslist.repositories.GameRepository;
 
 @Service 
 public class GameListService {
 
 	@Autowired 
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> buscarTodosOsGames(){
@@ -24,5 +28,20 @@ public class GameListService {
 		
 	}
 	
+	@Transactional
+	public void moverFilme(Long idList, int indiceOrigem, int indiceDestino) {
+		List<ProjetoGame> lista = gameRepository.pesquisaPorLista(idList);
+		
+		ProjetoGame obj = lista.remove(indiceOrigem);
+		lista.add(indiceDestino,obj);
+		
+		int min = indiceOrigem < indiceDestino ? indiceOrigem : indiceDestino;
+		int max = indiceOrigem < indiceDestino ? indiceDestino : indiceOrigem;
+		
+		for(int i = min; i<= max; i++) {
+			gameListRepository.atualizarPosicao(idList, lista.get(i).getId(), i);
+		}
+		
+	}
 	
 }
